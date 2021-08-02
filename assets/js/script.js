5555a73973c99734b4a4ca7fba0ea8101eddf660
Robson Oliveira <rosoliveira@uolinc.com>
2021-08-02 17:57:08 -0300

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+class LandingPageElo7 {
+
+    constructor() {}
+
+    /**
+     * Método inicializador da classe
+     *
+     * @memberof LandingPageElo7
+     * @method onInit
+     */
+    onInit() {}
+    
+}
+
+// Instância da classe
+const landingPageElo7 = new landingPageElo7;
+
+landingPageElo7.onInit();
